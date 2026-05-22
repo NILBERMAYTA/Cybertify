58b00b36b42f87f8866eb19f9e20d88c82497b79
Nilber <nilberrmayta@gmail.com>
2026-05-22 13:32:47 -0400

    Solucion de conflictos

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,31 +9,9 @@ interface ImportMeta {
   readonly env: ImportMetaEnv
 }
 
-type SpotifyWebPlaybackError = {
-  message: string
-}
-
-type SpotifyWebPlaybackReadyEvent = {
-  device_id: string
-}
-
-type SpotifyWebPlaybackPlayer = {
-  addListener: (event: string, callback: (event: SpotifyWebPlaybackError | SpotifyWebPlaybackReadyEvent) => void) => void
-  connect: () => Promise<boolean>
-  disconnect: () => void
-}
-
 interface Window {
   __CYBERTIFY_ENV__?: {
     VITE_SPOTIFY_CLIENT_ID?: string
     VITE_SPOTIFY_REDIRECT_URI?: string
-  }
-  onSpotifyWebPlaybackSDKReady?: () => void
-  Spotify?: {
-    Player: new (options: {
-      getOAuthToken: (callback: (token: string) => void) => void
-      name: string
-      volume?: number
-    }) => SpotifyWebPlaybackPlayer
   }
 }
